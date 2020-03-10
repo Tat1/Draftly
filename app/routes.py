e9da4c3e7f6d385de90a9d1a5c21a3d8aa6d6ce8
Tatiana Disu <t.disu@students.rave.ac.uk>
2020-03-10 15:01:43 +0000

    Structure of the Homepage

--- app/routes.py
+++ app/routes.py
@@ -3,4 +3,17 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return"Hello, World!"
+    user = {'username':'Miguel'}
+    return'''
+<HTML>
+    <HEAD>
+        <TITLE>Home Page -Draft</TITLE>
+    </HEAD>
+    <BODY>
+        <H1>Draft</H1>
+        <H2>Name your Story</H2>
+        <H3>Choose your plotline</H3>
+        <H3>Charcters</H3>
+        <H3>Roles</H3>
+    </BODY>
+</HTML>'''
