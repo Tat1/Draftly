1de5f52c9fff60f0ad1e6b2b72c831ebaef68dd1
Tatiana Disu <t.disu@students.rave.ac.uk>
2020-03-10 15:08:22 +0000

    moved the HTML to a template index page and rendered this to main

--- app/routes.py
+++ app/routes.py
@@ -1,19 +1,8 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'username':'Miguel'}
-    return'''
-<HTML>
-    <HEAD>
-        <TITLE>Home Page -Draft</TITLE>
-    </HEAD>
-    <BODY>
-        <H1>Draft</H1>
-        <H2>Name your Story</H2>
-        <H3>Choose your plotline</H3>
-        <H3>Charcters</H3>
-        <H3>Roles</H3>
-    </BODY>
-</HTML>'''
+    return render_template('index.html', title='Home')
