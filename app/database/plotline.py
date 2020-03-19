7b811a21e797e20c9df007aa0b8955c9edd00861
Tatiana Disu <t.disu@students.rave.ac.uk>
2020-03-19 18:46:06 +0000

    Update plotline.py

--- app/database/plotline.py
+++ app/database/plotline.py
@@ -11,14 +11,14 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 from sqlalchemy import Column, Integer, String
-class User(Base) :
-    __tablename__ = 'users'
+class Plotline(Base) :
+    __tablename__ = 'Plotline'
     
     id = Column(Integer, primary_key=True)
     name = Column(String)
     
     def __repr__(self):
-        return "<User(name='%s')>" % (self.name)
+        return "<Plotline(name='%s')>" % (self.name)
 
 Base.metadata.create_all(engine)
 
@@ -27,10 +27,81 @@ Session = sessionmaker(bind=engine)
 
 session = Session()
 
-ed_user = User(name='Rebirth')
+ed_user = Plotline(name='Rebirth')
 session.add(ed_user)
 session.commit()
 
-ed_user = User(name='Comedy')
+ed_user = Plotline(name='Comedy')
 session.add(ed_user)
 session.commit()
+
+ed_user = Plotline(name='Voyage and Return')
+session.add(ed_user)
+session.commit()
+
+ed_user = Plotline(name='The Quest')
+session.add(ed_user)
+session.commit()
+
+ed_user = Plotline(name='Tragedy')
+session.add(ed_user)
+session.commit()
+
+ed_user = Plotline(name='Overcoming the Monster')
+session.add(ed_user)
+session.commit()
+
+
+class Role(Base) :
+    __tablename__ = 'Role'
+    
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    
+    def __repr__(self):
+        return "<Role(name='%s')>" % (self.name)
+    
+Base.metadata.create_all(engine)
+
+from sqlalchemy.orm import sessionmaker
+Session = sessionmaker(bind=engine)
+
+session = Session()
+
+ed_user = Role(name='Antihero')
+session.add(ed_user)
+session.commit()
+
+ed_user = Role(name='Confidant')
+session.add(ed_user)
+session.commit()
+
+ed_user = Role(name='Love Interest')
+session.add(ed_user)
+session.commit()
+
+ed_user = Role(name='Tertiary Character')
+session.add(ed_user)
+session.commit()
+
+ed_user = Role(name='Deuteragonist')
+session.add(ed_user)
+session.commit()
+
+ed_user = Role(name='Foil')
+session.add(ed_user)
+session.commit()
+
+ed_user = Role(name='Quest Giver')
+session.add(ed_user)
+session.commit()
+
+ed_user = Role(name='Antagonist')
+session.add(ed_user)
+session.commit()
+
+ed_user = Role(name='Protagonist')
+session.add(ed_user)
+session.commit()
+
+
