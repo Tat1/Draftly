fce2fafdb81ddd2842f84aa3e55e14882026f810
Tatiana Disu <t.disu@students.rave.ac.uk>
2020-05-08 23:49:30 +0100

    Trying to sort out linking character creation to current story

--- app/storynameform.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
-
-class LoginForm(FlaskForm):
-    storyname = StringField('Name', validators=[DataRequired()])
-    submit = SubmitField('Submit')
