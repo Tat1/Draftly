f7635dfe7b78acdb6778c5184556eaf38e81b6a1
Tatiana Disu <t.disu@students.rave.ac.uk>
2020-05-27 09:17:00 +0100

    Trying the Plotline

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 theCharacters = []
+theOvercoming = []
 
 #Using Jinja
 @app.route('/',  methods = ['GET'])
@@ -11,11 +12,25 @@ def startup():
 
 @app.route('/newstory', methods = ['POST'])
 def newstory():
-    return render_template('newstory.html', characters=theCharacters)
+    AnticipationName = request.form["PlotlineControlSelect"]
+    DreamName = request.form["PlotlineControlSelect"]
+    FrustrationName = request.form["PlotlineControlSelect"]
+    NightmareName = request.form["PlotlineControlSelect"]
+    MiraculousEscapeName = request.form["PlotlineControlSelect"]
+        # Store as a Python dictionary:
+    theOvercoming.append({'anticipation' : AnticipationName,
+                          'dream' : DreamName,
+                          'frustration' : FrustrationName,
+                          'nightmare' : NightmareName,
+                          'miraculous escape' : MiraculousEscapeName,
+                         })
+
+    return render_template('newstory.html',PlotlineControlSelect=request.form['PlotlineControlSelect'], characters=theCharacters, overcoming=theOvercoming)
 
 @app.route('/storyparagraphs', methods = ['POST'])
 def storyparagraphs():
-    return render_template('storyparagraphs.html', StorynameFormControlInput=request.form['StorynameFormControlInput'], PlotlineControlSelect=request.form['PlotlineControlSelect'])
+    return render_template('storyparagraphs.html', StorynameFormControlInput=request.form['StorynameFormControlInput'], PlotlineControlSelect=request.form['PlotlineControlSelect'], characters=theCharacters, overcoming=theOvercoming)
+
 
 @app.route('/loadstory')
 def loadstory():
@@ -33,3 +48,6 @@ def charactercreation():
                           'arche' : archetypeName})
 
     return render_template('newstory.html', characters=theCharacters)
+
+
+
